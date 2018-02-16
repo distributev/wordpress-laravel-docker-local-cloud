67ab72e54169e782e8a2ba957dd1fbeeaa968269
Elghazal Ahmed <geniousphp@gmail.com>
2018-02-16 01:13:28 +0000

    configure apache && install sage && run sage webpack watcher as container

--- setup/wordpress/init.sh
+++ setup/wordpress/init.sh
@@ -1,15 +1,15 @@
 #This init script will generate new bedrock project and initialize wordpress
 #!/bin/bash
-set -eax
+set -ax
 
 WP_INIT=${WP_INIT:-"no"}
-PATH_PROJECT="/var/www/html"
+SAGE_THEME_NAME=${SAGE_THEME_NAME:-"sage"}
 
 if [ ${WP_INIT} = "yes" ]; then
   # install bedrock & set permissions
   echo "Installing bedrock..."
   chown -R www-data:www-data . && chmod g+s .
-  gosu www-data composer create-project roots/bedrock . && chmod -R 755 .
+  gosu www-data composer create-project roots/bedrock .
 
   if [ -n "$ADMIN_USER" ] && [ -n "$ADMIN_PASS" ] && [ -n "$ADMIN_EMAIL" ] && [ -n "$SITE_TITLE" ]; then
     sync # see: https://github.com/docker/docker/issues/9547
@@ -24,4 +24,11 @@ if [ ${WP_INIT} = "yes" ]; then
       --skip-email
   fi
 
+  gosu www-data composer create-project roots/sage web/app/themes/${SAGE_THEME_NAME} dev-master
+  yarn --cwd=web/app/themes/${SAGE_THEME_NAME}
+
+  cd web/app/themes/${SAGE_THEME_NAME} && yarn run build
+
+  cp -r /tmp/wordpress/. ${APP_PATH}/ #Copy Dockerfile and .dockerignore to root project
+  chmod -R 777 .
 fi
---
+++ setup/wordpress/wait.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#This init script will Compile assets when file changes are made, start Browsersync session
+
+set -e
+
+while ! nc -z wordpress 80; do
+  echo "Waiting for wordpress service to start..."
+  sleep 2
+done
+
+exec "$@"
