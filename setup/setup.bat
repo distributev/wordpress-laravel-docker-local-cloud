9023bc5f16734ffb7a3cea099e4ce4a87a6fe33a
distributev <distributev@gmail.com>
2018-02-16 12:35:32 +0100

    implement setup.bat

---
+++ setup/setup.bat
@@ -0,0 +1,12 @@
+setlocal
+
+set _path=%~f0
+
+for %%a in ("%_path%") do set "p_dir=%%~dpa"
+for %%a in (%p_dir:~0,-1%) do set "p2_dir=%%~dpa"
+
+set PROJECT_PATH=%p2_dir%work
+
+set WP_INIT=yes
+
+docker-compose up
